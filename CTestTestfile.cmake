69f9337c3929f89e2ac5f94e2e86d89e924cee32
Sergey Chikin <sergey.chikin@gmail.com>
2019-08-25 17:25:27 +0000

    dirty commit (cleanup later)

---
+++ CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /home/vbccam/kms-omni-build
+# Build directory: /home/vbccam/kms-omni-build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("kms-cmake-utils")
+subdirs("kms-jsonrpc")
+subdirs("kms-core")
+subdirs("kms-elements")
+subdirs("kms-filters")
+subdirs("kurento-media-server")
